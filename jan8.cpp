ab59622aa894966ff61c824fecb7f830bf65858f
PJ <peterjoe2005@gmail.com>
2025-01-09 13:47:52 -0500

    updated jan8

--- jan8.cpp
+++ jan8.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main(){
-  cout << sum_of_odd_nums() << endl;
-  cout << sum_of_digits(137) << endl;
+  cout << "sum of odd digits from 0 to 100 is: " << sum_of_odd_nums() << endl;
+  cout << "sum of the digits in the number 137 is: " << sum_of_digits(137) << endl;
   return 0;
 }
